e11531c046fa06e3a679cc696aec8cdf102a7f97
sumhillj <sumhillj@gmail.com>
2025-03-12 21:15:07 +0900

    feat: 홈페이지 ui 제작

---
+++ src/components/CourseTag/CourseTag.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./CourseTag.module.css";
+import { 
+  AiOutlineCheckCircle, 
+  AiOutlineExclamationCircle, 
+  AiOutlineClockCircle, 
+  AiOutlineQuestionCircle 
+} from "react-icons/ai";
+
+const CourseTag = ({ type, count }) => {
+  let bgColor, textPrefix, Icon;
+
+  switch (type) {
+    case "all":
+      bgColor = "#5C4F43";
+      textPrefix = "탐색할 명소";
+      Icon = AiOutlineCheckCircle;
+      break;
+    case "special":
+      bgColor = "#38A169";
+      textPrefix = "특별 관광요소";
+      Icon = AiOutlineExclamationCircle;
+      break;
+    case "landmark":
+      bgColor = "#D4A017";
+      textPrefix = "역사 랜드마크";
+      Icon = AiOutlineClockCircle;
+      break;
+    case "mission":
+      bgColor = "#C53030";
+      textPrefix = "미션 관광요소";
+      Icon = AiOutlineQuestionCircle;
+      break;
+    default:
+      bgColor = "#38A169";
+      textPrefix = "특별 관광요소";
+      Icon = AiOutlineExclamationCircle;
+  }
+
+  return (
+    <div className={styles.TagContainer} style={{ background: bgColor }}>
+      <Icon className={styles.icon} />
+      <p className={styles.TagText}>{`${textPrefix} ${count}개`}</p>
+    </div>
+  );
+};
+
+export default CourseTag;
