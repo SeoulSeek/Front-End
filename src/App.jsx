2da264100ee1274cff574ce4d70d389a6884142f
yesyes-12 <inbeom1202@gmail.com>
2025-03-18 23:27:57 +0900

    init: 관광명소 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./layouts/layout";
 import Home from "./pages/HomePage/HomePage";
 import Login from "./pages/LoginPage/LoginPage";
 import Signin from "./pages/SigninPage/SigninPage";
+import Places from "./pages/PlacesPage/PlacesPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/places" element={<Places />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signin" element={<Signin />} />
---
+++ src/pages/PlacesPage/PlacesPage.jsx
@@ -0,0 +1,8 @@
+// ./src/pages/PlacesPage/PlacesPage
+import React from "react";
+
+const Places = () => {
+  return <div>관광명소 페이지입니다.</div>;
+};
+
+export default Places;
