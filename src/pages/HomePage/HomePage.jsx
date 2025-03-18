95ccc96a897c28155c9bc83644a1d29fa2722523
yesyes-12 <inbeom1202@gmail.com>
2025-03-18 22:29:47 +0900

    feat: 로딩화면 ui 제작

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,9 +7,11 @@ import foldingMap from "../../assets/HomePage/foldingMap.png";
 import glass from "../../assets/HomePage/glass.png";
 import dummy1 from "../../assets/HomePage/dummy1.jpg";
 import dummy2 from "../../assets/HomePage/dummy2.jpg";
+import Loading from "../../components/Loading/Loading";
 
 const Home = () => {
   const [timePeriod, setTimePeriod] = useState("오후");
+  const [isLoading, setIsLoading] = useState(true);
 
   const toggleTimePeriod = () => {
     setTimePeriod((prev) => (prev === "오후" ? "오전" : "오후"));
@@ -28,24 +30,23 @@ const Home = () => {
         ‘서울식’이라는 표현은 단순히 서울만의 스타일이나
         <br />
         분위기를 의미하는 것을 넘어,
-        <br />
-        이 도시가 지닌 특별한 정체성과 개성을 나타냅니다.
+        <br />이 도시가 지닌 특별한 정체성과 개성을 나타냅니다.
       </p>
       <div className={styles.recommContainer}>
         <div className={styles.recommBox}>
           <h2 className={styles.h2}>금주의 추천 관광코스</h2>
           <div className={styles.recommInnerBox}>
             <div className={styles.tag1}>
-              <CourseTag type="all" count={7}/>
+              <CourseTag type="all" count={7} />
             </div>
             <div className={styles.tag2}>
-              <CourseTag type="special" count={2}/>
+              <CourseTag type="special" count={2} />
             </div>
             <div className={styles.tag3}>
-              <CourseTag type="landmark" count={4}/>
+              <CourseTag type="landmark" count={4} />
             </div>
             <div className={styles.tag4}>
-              <CourseTag type="mission" count={1}/>
+              <CourseTag type="mission" count={1} />
             </div>
             <img
               alt="추천 관광코스 이미지"
