86e537ae7077743d5ba9bed3b89e92bdc4290c12
sumhillj <sumhillj@gmail.com>
2025-05-07 16:13:13 +0900

    feat: 유저 정보 ui 구현

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import styles from "./MyPage.module.css";
+import defaultProfile from "../../assets/MyPage/defaultProfile.png"
 
 const MyPage = () => {
   return (
     <>
-      hello.
+      <div className={styles.myContainer}>
+        <div className={styles.myInfoContainer}>
+          <img src={defaultProfile} className={styles.profilePic} alt="프로필 이미지" />
+          <div className={styles.myInfo}>
+            <span className={styles.myName}>
+              <span className={styles.blue}>서우리</span>님의 역사 탐험록
+            </span>
+            <div className={styles.myLang}>
+              <span className={styles.lang}>한국어</span>
+              <span className={styles.lang}>English</span>
+              <span className={styles.lang}>中國語</span>
+            </div>
+            <div className={styles.myStats}>
+              <span className={styles.stats}>작성한 방명록 수 100</span>
+              <span className={styles.stats}>누적 좋아요 수 50</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
