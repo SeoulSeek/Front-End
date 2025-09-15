e4f0f1a231d35e6687717da769d9f1ec49c3df2e
여인범 <iamnotronaldo@kau.kr>
2025-09-15 17:34:27 +0900

    feat: PlacesPage반응형 작업

--- src/pages/PlacesPage/PlacesPage.jsx
+++ src/pages/PlacesPage/PlacesPage.jsx
@@ -1,5 +1,5 @@
 // ./src/pages/PlacesPage/PlacesPage
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { AiOutlineEdit } from "react-icons/ai";
 
@@ -9,14 +9,13 @@ import PostBox from "../../components/PostBox/PostBox";
 import Checkbox from "../../components/Checkbox/Checkbox";
 import SortMenu from "../../components/global/SortMenu/SortMenu";
 import dummyPosts from "../../data/dummyPosts";
-
-const ITEMS_PER_PAGE = 3;
+import seoulMap from "../../assets/PlacePage/seoulMap.jpg";
 
 const Places = () => {
   const [showAdminPosts, setShowAdminPosts] = useState(true);
   const [showUserPosts, setShowUserPosts] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(dummyPosts.length / ITEMS_PER_PAGE);
+  const [itemsPerPage, setItemsPerPage] = useState(3); // default: 모바일
 
   // 검색어 서치
   const navigate = useNavigate();
@@ -27,16 +26,35 @@ const Places = () => {
     navigate("/places/edit");
   };
 
+  useEffect(() => {
+    const updateItemsPerPage = () => {
+      const width = window.innerWidth;
+      if (width >= 1024) {
+        setItemsPerPage(9); // PC
+      } else if (width >= 768) {
+        setItemsPerPage(6); // 태블릿
+      } else {
+        setItemsPerPage(3); // 모바일
+      }
+    };
+
+    updateItemsPerPage(); // 처음 실행
+    window.addEventListener("resize", updateItemsPerPage);
+
+    return () => window.removeEventListener("resize", updateItemsPerPage);
+  }, []);
+
   // 유저, 관리자 게시물 필터링
   const filteredPosts = dummyPosts.filter((post) => {
     const isAdminPost = post.author.admin;
     return (showAdminPosts && isAdminPost) || (showUserPosts && !isAdminPost);
   });
 
+  const totalPages = Math.ceil(dummyPosts.length / itemsPerPage);
   // 현재 페이지 게시물
   const currentPosts = filteredPosts.slice(
-    (currentPage - 1) * ITEMS_PER_PAGE,
-    currentPage * ITEMS_PER_PAGE
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
   );
 
   return (
@@ -44,53 +62,56 @@ const Places = () => {
       <div className={$.placesPage}>
         <div className={$.titleWrap}>
           <h1 className={$.pageTitle}>서울식 관광명소</h1>
-        </div>
-
-        <div className={$.searchWrap}>
-          <SearchBar
-            onSearch={handleSearch}
-            placeholder="자치구, 키워드, 유저명으로 방명록을 검색해보세요"
-          />
-          <div className={$.filterContainer}>
-            <Checkbox
-              checked={showAdminPosts}
-              onChange={() => setShowAdminPosts(!showAdminPosts)}
-            >
-              관리자 추천 명소만 보기
-            </Checkbox>
-            <Checkbox
-              checked={showUserPosts}
-              onChange={() => setShowUserPosts(!showUserPosts)}
-            >
-              유저 추천 명소만 보기
-            </Checkbox>
+          <div className={$.searchWrap}>
+            <SearchBar
+              onSearch={handleSearch}
+              placeholder="자치구, 키워드, 유저명으로 방명록을 검색해보세요"
+            />
+            <div className={$.filterContainer}>
+              <Checkbox
+                checked={showAdminPosts}
+                onChange={() => setShowAdminPosts(!showAdminPosts)}
+              >
+                관리자 추천 명소만 보기
+              </Checkbox>
+              <Checkbox
+                checked={showUserPosts}
+                onChange={() => setShowUserPosts(!showUserPosts)}
+              >
+                유저 추천 명소만 보기
+              </Checkbox>
+            </div>
           </div>
         </div>
 
-        <div className={$.mapWrap}>
-          <div className={$.map}>{/* 미니맵 지도 API */}</div>
-          <span className={$.message}>
-            서울식 유저들이 가장 많이 방문한
-            <br /> 역사 명소를 확인해보세요!
-          </span>
-        </div>
+        <div className={$.contentsWrap}>
+          <div className={$.mapWrap}>
+            <div className={$.map}>
+              {/* 미니맵 지도 API */}
+              <img src={seoulMap} />
+            </div>
+            <span className={$.message}>
+              서울식 유저들이 가장 많이 방문한 역사 명소를 확인해보세요!
+            </span>
+          </div>
 
-        <div className={$.postListWrap}>
-          <ul className={$.postList}>
-            {currentPosts.map((post) => (
-              <PostBox
-                key={post.id}
-                id={post.id}
-                title={post.title}
-                place={post.metadata.location.name}
-                district={post.metadata.location.district}
-                hashtags={post.metadata.tags}
-                username={post.author.username}
-                likes={post.stats.likes}
-                comments={post.comments.length}
-              />
-            ))}
-          </ul>
+          <div className={$.postListWrap}>
+            <ul className={$.postList}>
+              {currentPosts.map((post) => (
+                <PostBox
+                  key={post.id}
+                  id={post.id}
+                  title={post.title}
+                  place={post.metadata.location.name}
+                  district={post.metadata.location.district}
+                  hashtags={post.metadata.tags}
+                  username={post.author.username}
+                  likes={post.stats.likes}
+                  comments={post.comments.length}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
 
         <div className={$.postCreateBtnWrap}>
